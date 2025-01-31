07de6bfdf8ec19473b423dc487f81495c714a75d
rohnshrma <rohn11shrma@gmail.com>
2025-01-31 16:51:02 +0530

    Add Navbar component and integrate Bootstrap for styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Menu from "./Pages/Menu";
 import Cart from "./Pages/Cart";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      {/* navbar */}
+      <Navbar />
 
       <Routes>
         <Route path="/" element={<Home />} />
